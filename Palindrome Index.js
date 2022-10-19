89d00309d790323cd3f6144b5a789eacaa03dd6c
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-10-19 13:18:28 +0530

    Update Palindrome Index.js

--- Palindrome Index.js
+++ Palindrome Index.js
@@ -27,3 +27,46 @@ function palindromeIndex(s) {
     
     return -1;
 }
+
+
+
+// Alternate Solution
+
+
+function palindromeIndex(s) {
+    // Write your code here
+       let mid   = Math.floor(s.length / 2);
+    let start  = s.slice(0, mid);
+    let end    = s.slice(0 - mid).split('').reverse().join('');
+
+    if(start === end){
+        return -1;
+    } else {
+        let position = 0;
+
+        while(position < start.length){
+            if(start[position] != end[position]){
+                break;
+            }
+            position++;
+        }
+
+        let edit_start = start.split('');
+        let edit_end   = end.split('');
+        let middle     = s[mid];
+
+        edit_start.splice(position, 1);
+        edit_end.splice(position, 1);
+
+        edit_start = edit_start.join('') + middle;
+        edit_end   = edit_end.join('') + middle;
+
+        if(start === edit_end){
+            return s.length - position - 1;
+        } else if(edit_start === end){
+            return position;
+        } else {
+            return -1;
+        }
+    }
+}
